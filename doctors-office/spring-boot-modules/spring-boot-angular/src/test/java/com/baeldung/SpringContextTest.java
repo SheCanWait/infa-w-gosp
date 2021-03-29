89f533c0f27a573381f270479abe3e741c868e5a
SheCanWait <krolu98@gmail.com>
2021-03-30 00:22:46 +0200

    h2 database migrated to embedded version and ecommerce module deleted

--- doctors-office/spring-boot-modules/spring-boot-angular/src/test/java/com/baeldung/SpringContextTest.java
+++ doctors-office/spring-boot-modules/spring-boot-angular/src/test/java/com/baeldung/SpringContextTest.java
@@ -1,14 +1,13 @@
 package com.baeldung;
 
+import com.baeldung.application.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.baeldung.ecommerce.EcommerceApplication;
-
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = EcommerceApplication.class)
+@SpringBootTest(classes = Application.class)
 public class SpringContextTest {
 
     @Test
